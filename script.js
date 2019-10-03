937f4e85a25d135edd04710fd078e5836c726560
Irina <pyx008@mail.ru>
2019-10-03 12:56:06 +0300

    Fix hometask8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var x = prompt("Укажите число", 3);
-var y = 3;
+var x = prompt("Укажите число x", 3);
+var y = prompt("Укажите число y", 4);
 var xNumber = parseInt (x, 10);
-var sum = xNumber + y;
+var yNumber = parseInt (y, 10);
+var sum = xNumber + yNumber;
 alert (sum);
