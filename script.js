0b2cdb23a3004ed96124c4cb4eba22f8b6b81660
Irina <pyx008@mail.ru>
2019-10-16 11:10:24 +0300

    Fix hometask10

--- script.js
+++ script.js
@@ -6,29 +6,26 @@ function checkForm(element){
 	if (value === ''){
 		element.style.borderBottom = "5px solid red"; 
 		return false;
-
 	} 
 	else{
 		element.style.borderBottom = "5px solid rgba(0, 0, 0, 0.6)"; 
 		return true;
 	}
 }
-function validate(event) {
+function validate(event){
 	event.preventDefault();
-	var elementsForm = document.getElementsByClassName("form__input_required");
+	var elementsForm = document.getElementsByClassName("required");
 
 	var correctElements=0;
-	for (var i = 0; i<elementsForm.length; i++) {
+	for (var i = 0; i<elementsForm.length; i++){
 		const isCorrect=checkForm(elementsForm[i]);
 		if(isCorrect===true){
 			correctElements++;
 		}
 	}
 	if (correctElements===elementsForm.length){
-		console.log(correctElements);
-		document.getElementById('thanks').innerHTML="Спасибо, ваша заявка отправлена!";
+		//console.log(correctElements);
+		document.getElementById('pop-upWindow').innerHTML="Спасибо, ваша заявка отправлена!";
 	}
-
 }
 form.addEventListener('submit', validate);
-
